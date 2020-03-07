d8149dbbd8ab0d44da31346638c726a15ea78b3d
xuanzhiqiang <xuanzhiqiang01@163.com>
2020-03-07 22:42:50 +0800

    string

--- ch06/instructions/constants/ldc.go
+++ ch06/instructions/constants/ldc.go
@@ -1,35 +1,47 @@
-package references
+package constants
 
 import (
 	"jvmgo/ch06/instructions/base"
 	"jvmgo/ch06/rtda"
+	"jvmgo/ch06/rtda/heap"
 )
 
+// Push item from run-time constant pool
 type LDC struct{ base.Index8Instruction }
-type LDC_W struct{ base.Index16Instruction }
-type LDC2_W struct{ base.Index16Instruction }
 
 func (self *LDC) Execute(frame *rtda.Frame) {
 	_ldc(frame, self.Index)
 }
+
+// Push item from run-time constant pool (wide index)
+type LDC_W struct{ base.Index16Instruction }
+
 func (self *LDC_W) Execute(frame *rtda.Frame) {
 	_ldc(frame, self.Index)
 }
 
 func _ldc(frame *rtda.Frame, index uint) {
 	stack := frame.OperandStack()
-	cp := frame.Method().Class().ConstantPool()
-	c := cp.GetConstant(index)
+	class := frame.Method().Class()
+	c := class.ConstantPool().GetConstant(index)
 
 	switch c.(type) {
 	case int32:
 		stack.PushInt(c.(int32))
 	case float32:
 		stack.PushFloat(c.(float32))
+	case string:
+		internedStr := heap.JString(class.Loader(), c.(string))
+		stack.PushRef(internedStr)
+	// case *heap.ClassRef:
+	// case MethodType, MethodHandle
 	default:
 		panic("todo: ldc!")
 	}
 }
+
+// Push long or double from run-time constant pool (wide index)
+type LDC2_W struct{ base.Index16Instruction }
 
 func (self *LDC2_W) Execute(frame *rtda.Frame) {
 	stack := frame.OperandStack()
@@ -42,6 +54,6 @@ func (self *LDC2_W) Execute(frame *rtda.Frame) {
 	case float64:
 		stack.PushDouble(c.(float64))
 	default:
-		panic("todo: ldc!")
+		panic("java.lang.ClassFormatError")
 	}
 }
